ecc347c84835ee67cd9fa7435465438b9469d6d3
sataylor1017 <sataylor1017@gmail.com>
2018-06-12 11:55:29 -0400

    removed sample assets

---
+++ Assets/Scripts/cameraController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour {
+
+    public GameObject player;
+    public float screenPercentToScroll; // Last n-percent of screen scrolls
+    public float maxOffset; // max screen offset
+    public float scrollRate; // screen scroll rate
+
+    private Vector3 offset;
+    private Vector3 mousePosition;
+
+	// Use this for initialization
+	void Start () {
+        offset = new Vector3(0, 0, -10); //transform.position - player.transform.position;
+        Debug.Log(string.Concat(offset.x.ToString(), offset.y.ToString(), offset.z.ToString()));
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        // Horizontal scroll check
+        if ((mousePosition.x > Screen.width * (1-(screenPercentToScroll/100))) && offset.x < maxOffset)
+        {
+            //Scroll camera right
+            offset = offset + new Vector3 (scrollRate * Time.deltaTime, 0,0);
+        }
+        else if ((mousePosition.x < Screen.width * (screenPercentToScroll/100)) && offset.x > -maxOffset)
+        {
+            //Scroll camera left
+            offset = offset + new Vector3(-scrollRate * Time.deltaTime, 0, 0);
+        }
+
+        // Vertical scroll check
+        if (mousePosition.y > Screen.height * (1 - (screenPercentToScroll / 100)) && offset.y < maxOffset)
+        {
+            //Scroll camera up
+            offset = offset + new Vector3(0, scrollRate*Time.deltaTime, 0);
+        }
+        else if (mousePosition.y < Screen.height * (screenPercentToScroll / 100) && offset.y > -maxOffset)
+        {
+            //Scroll camera down
+            offset = offset + new Vector3(0, -scrollRate * Time.deltaTime, 0);
+        }
+
+        transform.position = player.transform.position + offset;
+        mousePosition = Input.mousePosition;
+    }
+}
---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+
+    public int playerSpeed = 10;
+
+    private Vector3 moveXY;
+    private Vector2 mousePos;
+    private Rigidbody2D rb2d;
+    private Vector3 mousePosition;
+    private Vector3 lookVector;
+    private float angle; // Rotation angle of player
+
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D> ();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Player_Move();
+    }
+
+
+    void Player_Move()
+    {
+
+        //Player Movement
+        moveXY = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        mousePos = Input.mousePosition;
+        rb2d.AddForce(moveXY * playerSpeed);
+
+        //Player Rotation
+        mousePosition = Camera.main.ScreenToWorldPoint(new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0.0f));
+        lookVector = mousePosition - transform.position;
+        angle = Mathf.Atan2(lookVector.y, lookVector.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+
+}
