3938f1d4fb18af232bb8b4244b00e87b9ebb90f9
sataylor1017 <sataylor1017@gmail.com>
2018-06-20 23:44:35 -0400

    added gun raycasting

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -6,6 +6,8 @@ public class playerController : MonoBehaviour
 {
 
     public int playerSpeed;
+    public float shootDebounce;
+    public float shootDistance;
 
     private Vector3 moveXY;
     private Vector2 mousePos;
@@ -13,6 +15,10 @@ public class playerController : MonoBehaviour
     private Vector3 mousePosition;
     private Vector3 lookVector;
     private float angle; // Rotation angle of player
+    private float lastShot = 0;
+    private float trailDebounce = 0.02f;
+    private Vector3 endPoint;
+
 
     // Use this for initialization
     void Start()
@@ -24,22 +30,53 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        lookVector = mousePosition - transform.position;
         Player_Move();
+
+        //Check for mouse button 0 keypress
+        if (Input.GetKeyDown(KeyCode.Mouse0) && (Time.fixedTime - lastShot) >= shootDebounce) //MB1 and debounce time is passed
+        {
+            Shoot();
+        }
+        
     }
 
 
     void Player_Move()
     {
-
         //Player Movement
         moveXY = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         rb2d.velocity = (moveXY * playerSpeed);
+        rb2d.angularVelocity = 0;
 
         //Player Rotation
         mousePosition = Camera.main.ScreenToWorldPoint(new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0.0f));
-        lookVector = mousePosition - transform.position;
         angle = Mathf.Atan2(lookVector.y, lookVector.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+
+    void Shoot()
+    {
+        lastShot = Time.fixedTime; //
+        //print(LayerMask.NameToLayer("Player"));
+        int layermask1 = 1 << LayerMask.NameToLayer("Player"); //Binary bit-shift
+        int layermask2 = 1 << LayerMask.NameToLayer("Triggers");
+        int finalmask = layermask1 | layermask2; // Combine layerMasks
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right, shootDistance, ~(finalmask)); // raycast
+        if (hit.collider != null)
+        {
+            // HIT!
+            print(hit.point);
+            endPoint = hit.point; // endpoint at collision
+        }
+        else
+        {
+            // MISS!
+            endPoint = transform.position + shootDistance * transform.right; // endpoint at max dist
+        }
+        Vector3 startPoint = (transform.position + 1.2f * transform.right);
+        Debug.DrawRay(startPoint, (endPoint - startPoint), Color.yellow, trailDebounce); // draw preview
+
     }
 
 }
